8500cc0a4cbec3fb93c5c16173d73131f45f116d
TAMTA JOJUA <jojua.tamta99@gmail.com>
2022-11-05 20:53:50 +0400

    main page view is created

---
+++ src/app/feautures/main/components/driver-reg/driver-reg.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-driver-reg',
+  templateUrl: './driver-reg.component.html',
+  styleUrls: ['./driver-reg.component.scss']
+})
+export class DriverRegComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/feautures/main/main.module.ts
+++ src/app/feautures/main/main.module.ts
@@ -1,11 +1,14 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { MainRoutingModule } from './main-routing.module';
-import { MainPageComponent } from './components/main-page/main-page.component';
+import { MainRoutingModule } from "./main-routing.module";
+import { MainPageComponent } from "./components/main-page/main-page.component";
+import { TranslateModule } from "@ngx-translate/core";
+import { DriverRegComponent } from './components/driver-reg/driver-reg.component';
+import { FeaturesComponent } from './components/features/features.component';
 
 @NgModule({
-  declarations: [MainPageComponent],
-  imports: [CommonModule, MainRoutingModule],
+  declarations: [MainPageComponent, DriverRegComponent, FeaturesComponent],
+  imports: [CommonModule, MainRoutingModule, TranslateModule],
 })
 export class MainModule {}
